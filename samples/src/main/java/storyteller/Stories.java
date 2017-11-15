99409bc60d1993c10964bcee9327fe3d29c52bde
sassmith809 <sassmith809@gmail.com>
2017-11-15 17:32:40 -0500

    Adding SSML to Story Teller- GLITCHED
    Having SSML issues with lines 41-54

--- samples/src/main/java/storyteller/Stories.java
+++ samples/src/main/java/storyteller/Stories.java
@@ -28,6 +28,30 @@ public final class Stories {
         						+ "<emphasis level=\"reduced\">I did not even know you were there.</emphasis> </speak>", 
         				"<speak> <emphasis level=\"moderate\">We are often of greater importance in our own eyes than in the eyes of our neighbor. "
         				+ "The smaller the mind, the greater the conceit.</emphasis> </speak>"));
+        STORY_COLLECTION.add(
+				new AesopFable("The Ant and the Dove", 
+        				"<speak>" + "<emphasis level=\"moderate\"> A Dove saw an Ant fall into a brook. The Ant struggled in vain to reach the bank, "
+        						+ "and in pity, the Dove dropped a blade of straw close beside it. Clinging to the straw "
+        						+ "like a shipwrecked sailor to a broken spar, the Ant floated safely to shore. "
+        						+ "Soon after, the Ant saw a man getting ready to kill the Dove with a stone. "
+        						+ "But just as he cast the stone, </emphasis> the Ant stung him in the heel, "
+        						+ "so that the pain made him miss his aim, and the startled Dove flew to safety in a distant wood. </speak>" , 
+        				"<speak> <emphasis level=\"moderate\">" + "A kindness is never wasted." + "</emphasis> </speak>"));
+        
+        STORY_COLLECTION.add(
+        		new AesopFable("The Stag and His Reflection",
+        				"<speak> <prosody pitch =+50%> "
+        				+ "A Stag, drinking from a crystal spring, saw himself mirrored in the clear water. " 
+        				+ "He greatly admired the graceful arch  of his antlers, but he was very much ashamed of his spindling legs. " + "</prosody>"
+        				/* + "<prosody pitch =\"-33.3%\"> "+ "How can it be, " + "</prosody>"
+        				+ "<prosody pitch =\"+25%\"> " + " he sighed, " + "</prosody"
+        				+ "<prosody pitch =\"-33.3%\"> "+ " that I should be cursed with such legs when I have so magnificent a crown. " + "</prosody"
+        				+ "<prosody pitch =\"+25%\"> " + "At that moment he scented a panther, and in an instant was bounding away through the forest. "
+        				+ "But as he ran, his wide-spreading antlers caught in the branches of the trees, and soon the Panther overtook him. "
+        				+ "Then the Stag perceived that the legs of which he was so ashamed would have saved him, "
+        				+ "had it not been for the useless ornaments on his head. </prosody> </speak> " */,
+        			"<speak> <emphasis level=\"strong\">" + "We often make much of the ornamental and despise the useful." 
+        	+ " </emphasis> </speak>"	));
     } 
     
 	public static AesopFable getFable(int id) {
