6192d0f8ba2a9d57780151aaeffda9146ea0ce86
JoeyISS <joe.luo@indiansprings.org>
2017-11-16 12:07:57 -0600

    Submission for Project II

--- samples/src/main/java/storyteller/Stories.java
+++ samples/src/main/java/storyteller/Stories.java
@@ -18,7 +18,7 @@ public final class Stories {
         
         STORY_COLLECTION.add(
 				new AesopFable("The Gnat and the Bull", 
-        				"<speak>" + "A Gnat flew over the meadow with much buzzing for so small a creature and settled on the tip of one "
+        				"A Gnat flew over the meadow with much buzzing for so small a creature and settled on the tip of one "
         						+ "of the horns of a Bull. After he had rested a short time, he made ready to fly away. But before he left "
         						+ "he begged the Bull's pardon for having used his horn for a resting place. "
         						+ "<emphasis level=\"reduced\">You must be very glad to have me go now </emphasis>"
@@ -28,6 +28,43 @@ public final class Stories {
         						+ "<emphasis level=\"reduced\">I did not even know you were there.</emphasis> </speak>", 
         				"<speak> <emphasis level=\"moderate\">We are often of greater importance in our own eyes than in the eyes of our neighbor. "
         				+ "The smaller the mind, the greater the conceit.</emphasis> </speak>"));
+        STORY_COLLECTION.add(
+        		new AesopFable("The Miser",
+        				"<p><prosody volume=\"soft\">A Miser had buried his gold in a <amazon:effect name=\"whispered\"><prosody rate=\"slow\">secret place</prosody></amazon:effect> in his garden.</prosody> " +
+        							"<emphasis level=\"strong\">Every day</emphasis> he went to the spot, dug up the treasure and counted it <prosody rate=\"x-slow\">piece by piece</prosody> to make sure <emphasis level=\"moderate\">it was all there</emphasis>.</p> " +
+        							"He made <emphasis level=\"moderate\">so many trips</emphasis> that a Thief,<break time=\".5s\"/>  who had been observing him,<break time=\".8s\"/> guessed what it was the Miser had hidden," + 
+        							"and one night, <amazon:effect name=\"whispered\">quietly dug up the treasure and made off with it.</amazon:effect> " +
+        							"When the Miser discovered his loss, he was overcome with <prosody rate=\"slow\">grief and despair.</prosody> " + 
+        							"<emphasis level=\"strong\">He groaned and cried and tore his hair</emphasis>. " +
+        							"A passerby heard his cries and asked what had happened. " + 
+        							
+        							//Miser							
+        							"<prosody volume=\"loud\"><emphasis level=\"strong\">My gold! O my gold!</emphasis></prosody> " + 
+        							"Cried the Miser,<prosody volume=\"loud\"><emphasis level=\"moderate\"> wildly</emphasis></prosody>, " +
+        							"<prosody volume=\"x-loud\"><prosody rate=\"medium\">Someone has robbed me</prosody></prosody>! " +				
+        							//Miser
+        							
+        							//Passby						
+        							"<prosody volume=\"x-loud\">Your gold</prosody>! <prosody volume=\"loud\">There in that hole? Why did you put it there?</prosody>" +
+        							"Why did you not keep it in the house where you could easily get it when you had to buy things?" +
+        							//Passby
+        							
+        							//Miser
+        							"<prosody volume=\"loud\"><emphasis level=\"moderate\">Buy!</emphasis></prosody>" +
+        							"Screamed the Miser angrily. " +
+        							"Why, I never touched the gold. <emphasis level=\"reduced\">I couldn't think of spending any of it</emphasis>. " +
+        							//Miser
+        							
+        							"<p>The stranger picked up a large stone and<prosody rate=\"fast\"> threw it into the hole.</prosody></p> " +
+        							"If that is the case, " +
+        							"he said," +
+        							"cover up that stone.<break time=\".5s\"/> <prosody volume=\"loud\">It is worth just as much to you as the treasure you lost</prosody>!" +	
+        							"</speak>",
+        							
+        							//Moral
+        							"<speak><emphasis level=\"moderate\">A possession is worth no more than the use we make of it</emphasis>. </speak>"
+        							)
+        		);
     } 
     
 	public static AesopFable getFable(int id) {
--- samples/src/main/java/storyteller/StoryTellerSpeechlet.java
+++ samples/src/main/java/storyteller/StoryTellerSpeechlet.java
@@ -27,6 +27,14 @@ public class StoryTellerSpeechlet implements Speechlet {
      */
     private static final String CUSTOM_SLOT_STORY_NAME = "StoryName";
 
+    private static final String STAGE_NAME = "stage";
+    
+    private static final int BEGINNING_STAGE = 0;
+    
+    private static final int ASK_NAME_STAGE = 1;
+    
+    private static final String STORY_ID = "story";
+    
         
     @Override
     public void onSessionStarted(final SessionStartedRequest request, final Session session)
@@ -113,11 +121,10 @@ public class StoryTellerSpeechlet implements Speechlet {
         // Create the help message
         String speechText =
                 "You can tell me which story you would like to hear "
-                        + "by saying, tell me the story of The Gnat and the Bull. "
+                        + "by saying, tell me the story of blah blah blah. "
                         + "Now, which story would you like to hear?";
         String repromptText =
                 "Which story would you like to hear?";
-
         PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
         speech.setText(speechText);
         
@@ -154,23 +161,104 @@ public class StoryTellerSpeechlet implements Speechlet {
         
     	// get the name of the story from the custom slot (within the JSON request)
         Slot storySlot = intent.getSlot(CUSTOM_SLOT_STORY_NAME);
-        
-        if (storySlot != null && storySlot.getValue() != null) {
-            String storyName = storySlot.getValue();
-
-            // get the story content for the corresponding story
-            storyContent = Stories.getFableWithName(storyName).getStoryContent();
-	        
-            // create an appropriate SSML response
-	        SsmlOutputSpeech outputSpeech = new SsmlOutputSpeech();
-	        outputSpeech.setSsml(storyContent);
-	
-	        Reprompt reprompt = new Reprompt();
-	        reprompt.setOutputSpeech(null);
-	        SpeechletResponse response = SpeechletResponse.newAskResponse(outputSpeech, reprompt);
-	        return response;
-	        
-        } else {
+    	
+        if(storySlot != null && storySlot.getValue() != null && Stories.getFableWithName(storySlot.getValue()) != null) {
+        	
+        	if(session.getAttributes().containsKey(STAGE_NAME)) {
+        		
+        		if((Integer)session.getAttribute(STAGE_NAME) == BEGINNING_STAGE) {
+        			
+        			session.setAttribute(STAGE_NAME, ASK_NAME_STAGE);
+        			
+        			String storyName = storySlot.getValue();
+        			
+        			session.setAttribute(STORY_ID, storyName);
+    				
+    	            // get the story content for the corresponding story
+    	            storyContent = "<speak>" + Stories.getFableWithName(storyName).getStoryContent();
+    		        
+    	            // create an appropriate SSML response
+    		        SsmlOutputSpeech outputSpeech = new SsmlOutputSpeech();
+    		        outputSpeech.setSsml(storyContent);
+    		
+    		        Reprompt reprompt = new Reprompt();
+    		        reprompt.setOutputSpeech(null);
+    		        SpeechletResponse response = SpeechletResponse.newAskResponse(outputSpeech, reprompt);
+    		        return response;
+    		        
+        		}else if((Integer)session.getAttribute(STAGE_NAME) == ASK_NAME_STAGE) {
+        			
+        			String storyName = storySlot.getValue();
+        			
+        			if(((String)session.getAttribute(STORY_ID)).equals(storyName))
+        			{	
+        				String prefix = "<speak><p>You want to hear the story again? <emphasis level=\"strong\">No problem.</emphasis></p> ";
+        				
+        				session.setAttribute(STORY_ID, storyName);
+        				
+        	            // get the story content for the corresponding story
+        	            storyContent = prefix + Stories.getFableWithName(storyName).getStoryContent();
+        		        
+        	            // create an appropriate SSML response
+        		        SsmlOutputSpeech outputSpeech = new SsmlOutputSpeech();
+        		        outputSpeech.setSsml(storyContent);
+        		
+        		        Reprompt reprompt = new Reprompt();
+        		        reprompt.setOutputSpeech(null);
+        		        SpeechletResponse response = SpeechletResponse.newAskResponse(outputSpeech, reprompt);
+        		        return response;
+        		        
+        			}else {
+        				
+        				String prefix = "<speak><p>You want to hear another story? <emphasis level=\"strong\">Sure.</emphasis>" + 
+        				" Here is your story.</p> ";
+                				
+        				session.setAttribute(STORY_ID, storyName);
+        				
+        	            // get the story content for the corresponding story
+        	            storyContent = prefix + Stories.getFableWithName(storyName).getStoryContent();
+        		        
+        	            // create an appropriate SSML response
+        		        SsmlOutputSpeech outputSpeech = new SsmlOutputSpeech();
+        		        outputSpeech.setSsml(storyContent);
+        		
+        		        Reprompt reprompt = new Reprompt();
+        		        reprompt.setOutputSpeech(null);
+        		        SpeechletResponse response = SpeechletResponse.newAskResponse(outputSpeech, reprompt);
+        		        return response;
+        			}
+        		}else {
+                	PlainTextOutputSpeech outputSpeech = new PlainTextOutputSpeech();
+                	outputSpeech.setText("Sorry, I do not know what you are asking about. ");
+                	Reprompt reprompt = new Reprompt();
+                	reprompt.setOutputSpeech(outputSpeech);
+                	SpeechletResponse response = SpeechletResponse.newAskResponse(outputSpeech, reprompt);
+                	return response;
+        		}
+        	}else {
+        		
+        		String prefix = "<speak><p><prosody pitch=\"high\"><prosody rate=\"x-slow\">Ah</prosody></prosody>... <prosody pitch=\"low\"><prosody rate=\"fast\">No</prosody></prosody>. <break time=\"2s\"/>" + 
+        		"<prosody pitch=\"high\">Just</prosody><prosody rate=\"slow\"><prosody pitch=\"high\">kidding</prosody></prosody>, here is your <prosody rate=\"slow\"><prosody pitch=\"medium\">story</prosody></prosody>.</p> ";
+        		
+        		session.setAttribute(STAGE_NAME, ASK_NAME_STAGE);
+        		
+        		String storyName = storySlot.getValue();
+    			
+    			session.setAttribute(STORY_ID, storyName);
+				
+	            // get the story content for the corresponding story
+	            storyContent = prefix + Stories.getFableWithName(storyName).getStoryContent();
+		        
+	            // create an appropriate SSML response
+		        SsmlOutputSpeech outputSpeech = new SsmlOutputSpeech();
+		        outputSpeech.setSsml(storyContent);
+		
+		        Reprompt reprompt = new Reprompt();
+		        reprompt.setOutputSpeech(null);
+		        SpeechletResponse response = SpeechletResponse.newAskResponse(outputSpeech, reprompt);
+		        return response;
+        	}
+        }else {
         	
         	// if the story requested by the user could not be found, create an appropriate response
         	PlainTextOutputSpeech outputSpeech = new PlainTextOutputSpeech();
@@ -180,7 +268,6 @@ public class StoryTellerSpeechlet implements Speechlet {
         	SpeechletResponse response = SpeechletResponse.newAskResponse(outputSpeech, reprompt);
         	return response;
         }
-        
     }
 
     /**
@@ -199,24 +286,75 @@ public class StoryTellerSpeechlet implements Speechlet {
     	// get the name of the story from the custom slot (within the JSON request)
         Slot storySlot = intent.getSlot(CUSTOM_SLOT_STORY_NAME);
         
-        if (storySlot != null && storySlot.getValue() != null) {
-            String storyName = storySlot.getValue();
-                       
-            // get the story content for the corresponding story
-            storyMoral = Stories.getFableWithName(storyName).getStoryMoral();
-	        
-            // create an appropriate SSML response
-	        SsmlOutputSpeech outputSpeech = new SsmlOutputSpeech();
-	        outputSpeech.setSsml(storyMoral);
+        if (storySlot != null && storySlot.getValue() != null && Stories.getFableWithName(storySlot.getValue()) != null) {
+        	
+        	if(session.getAttributes().containsKey(STAGE_NAME)) {
+        		
+        		if(session.getAttributes().containsKey(STORY_ID)) {
+        			
+        			if(storySlot.getValue().equals((String)session.getAttribute(STORY_ID))) {
+        				
+        				if((Integer)session.getAttribute(STAGE_NAME) == BEGINNING_STAGE) {
+                			PlainTextOutputSpeech outputSpeech = new PlainTextOutputSpeech();
+                        	outputSpeech.setText("You haven't tell me the story yet! Just say tell me a story of blah blah blah. Now, which story you want to hear?");
+                        	Reprompt reprompt = new Reprompt();
+                        	reprompt.setOutputSpeech(outputSpeech);
+                        	SpeechletResponse response = SpeechletResponse.newAskResponse(outputSpeech, reprompt);
+                        	return response;
+                		}else if((Integer)session.getAttribute(STAGE_NAME) == ASK_NAME_STAGE) {
+                			String storyName = storySlot.getValue();
+                            
+                			session.setAttribute(STAGE_NAME, BEGINNING_STAGE);
+                			
+                            // get the story content for the corresponding story
+                            storyMoral = Stories.getFableWithName(storyName).getStoryMoral();
+                	        
+                            // create an appropriate SSML response
+                	        SsmlOutputSpeech outputSpeech = new SsmlOutputSpeech();
+                	        outputSpeech.setSsml(storyMoral);
+                	
+                	        Reprompt reprompt = new Reprompt();
+                	        reprompt.setOutputSpeech(null);
+                	        SpeechletResponse response = SpeechletResponse.newAskResponse(outputSpeech, reprompt);
+                	        return response;
+                		}else {
+                        	PlainTextOutputSpeech outputSpeech = new PlainTextOutputSpeech();
+                        	outputSpeech.setText("Sorry, I do not know what you are asking about.");
+                        	Reprompt reprompt = new Reprompt();
+                        	reprompt.setOutputSpeech(outputSpeech);
+                        	SpeechletResponse response = SpeechletResponse.newAskResponse(outputSpeech, reprompt);
+                        	return response;
+                		}
+        			}else {
+        				PlainTextOutputSpeech outputSpeech = new PlainTextOutputSpeech();
+                    	outputSpeech.setText("Sorry, I can only tell the moral after I say the story. " + 
+        				"And I have not yet told you that story. If you want to hear it, just say tell me the story of blah blah blah.");
+                    	Reprompt reprompt = new Reprompt();
+                    	reprompt.setOutputSpeech(outputSpeech);
+                    	SpeechletResponse response = SpeechletResponse.newAskResponse(outputSpeech, reprompt);
+                    	return response;
+        			}
 	
-	        Reprompt reprompt = new Reprompt();
-	        reprompt.setOutputSpeech(null);
-	        SpeechletResponse response = SpeechletResponse.newAskResponse(outputSpeech, reprompt);
-	        return response;
-	        
+        	}else {
+        		PlainTextOutputSpeech outputSpeech = new PlainTextOutputSpeech();
+            	outputSpeech.setText("You have not asked me to tell a story!");
+            	Reprompt reprompt = new Reprompt();
+            	reprompt.setOutputSpeech(outputSpeech);
+            	SpeechletResponse response = SpeechletResponse.newAskResponse(outputSpeech, reprompt);
+            	return response;
+        	}       
         } else {
         	
         	// if the story requested by the user could not be found, create an appropriate response
+        	PlainTextOutputSpeech outputSpeech = new PlainTextOutputSpeech();
+        	outputSpeech.setText("Just opened the app right? No worries, you will get more adapt while you use more. " +
+        			"So you haven't tell me the story yet! Just say tell me a story of blah blah blah. Now, which story you want to hear?");
+        	Reprompt reprompt = new Reprompt();
+        	reprompt.setOutputSpeech(outputSpeech);
+        	SpeechletResponse response = SpeechletResponse.newAskResponse(outputSpeech, reprompt);
+        	return response;
+        	}
+        }else {
         	PlainTextOutputSpeech outputSpeech = new PlainTextOutputSpeech();
         	outputSpeech.setText("Sorry, I couldn't find the moral of the story you requested. Please try naming another story.");
         	Reprompt reprompt = new Reprompt();
