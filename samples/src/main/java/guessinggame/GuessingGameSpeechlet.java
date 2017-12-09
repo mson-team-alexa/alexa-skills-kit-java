34471b0f05c904b41dad4ccd950629e1d1dcc46a
RoseHairTarantula <mavis@somewords.org>
2017-12-09 15:54:47 -0500

    SSML updates

--- samples/src/main/java/guessinggame/GuessingGameSpeechlet.java
+++ samples/src/main/java/guessinggame/GuessingGameSpeechlet.java
@@ -1,21 +1,9 @@
 package guessinggame;
-<<<<<<< Updated upstream
-=======
-/**
-Copyright 2014-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-
-Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
-
-    http://aws.amazon.com/apache2.0/
-
-or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
-*/
->>>>>>> Stashed changes
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import com.amazon.speech.slu.Intent;
+import com.amazon.speech.slu.Slot;
 import com.amazon.speech.speechlet.IntentRequest;
 import com.amazon.speech.speechlet.LaunchRequest;
 import com.amazon.speech.speechlet.Session;
@@ -24,98 +12,28 @@ import com.amazon.speech.speechlet.SessionStartedRequest;
 import com.amazon.speech.speechlet.Speechlet;
 import com.amazon.speech.speechlet.SpeechletException;
 import com.amazon.speech.speechlet.SpeechletResponse;
+import com.amazon.speech.ui.OutputSpeech;
 import com.amazon.speech.ui.PlainTextOutputSpeech;
 import com.amazon.speech.ui.Reprompt;
 import com.amazon.speech.ui.SimpleCard;
-<<<<<<< Updated upstream
-
-/**
- * This sample shows how to create a simple speechlet for handling speechlet requests.
- */
-public class GuessingGameSpeechlet implements Speechlet {
-    private static final Logger log = LoggerFactory.getLogger(GuessingGameSpeechlet.class);
-
-    @Override
-    public void onSessionStarted(final SessionStartedRequest request, final Session session)
-            throws SpeechletException {
-        log.info("onSessionStarted requestId={}, sessionId={}", request.getRequestId(),
-                session.getSessionId());
-        // any initialization logic goes here
-    }
-
-    @Override
-    public SpeechletResponse onLaunch(final LaunchRequest request, final Session session)
-            throws SpeechletException {
-        log.info("onLaunch requestId={}, sessionId={}", request.getRequestId(),
-                session.getSessionId());
-        return playGameResponse();
-    }
-    
-    @Override
-    public SpeechletResponse onIntent(final IntentRequest request, final Session session)
-            throws SpeechletException {
-        log.info("onIntent requestId={}, sessionId={}", request.getRequestId(),
-                session.getSessionId());
-
-        Intent intent = request.getIntent();
-        String intentName = (intent != null) ? intent.getName() : null;
-        
-// change based on intents 
-        if ("PlayGameIntent".equals(intentName)) {
-            return playGameResponse();
-        } else if ("AMAZON.HelpIntent".equals(intentName)) {
-            return getHelpResponse();
-        } else {
-            throw new SpeechletException("Invalid Intent");
-        }
-    }
-
-    @Override
-    public void onSessionEnded(final SessionEndedRequest request, final Session session)
-            throws SpeechletException {
-        log.info("onSessionEnded requestId={}, sessionId={}", request.getRequestId(),
-                session.getSessionId());
-        // any cleanup logic goes here
-    }
-    
-    private SpeechletResponse playGameResponse() {
-        String speechText = "Welcome to the Animal Sound Guessing Game";
-
-        // Create the plain text output.
-        PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
-        speech.setText(speechText);
-
-        return SpeechletResponse.newTellResponse(speech);
-    }
-
-    /**
-     * Creates a {@code SpeechletResponse} for the help intent.
-     *
-     * @return SpeechletResponse spoken and visual response for the given intent
-     */
-    private SpeechletResponse getHelpResponse() {
-        String speechText = "You can ask to play a game";
-
-        // Create the plain text output.
-        PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
-        speech.setText(speechText);
-
-        // Create reprompt
-        Reprompt reprompt = new Reprompt();
-        reprompt.setOutputSpeech(speech);
-
-        return SpeechletResponse.newAskResponse(speech, reprompt);
-    }
-}
-
-=======
 import com.amazon.speech.ui.SsmlOutputSpeech;
 
-/**
-* This sample shows how to create a simple speechlet for handling speechlet requests.
-*/
+
 public class GuessingGameSpeechlet implements Speechlet {
 private static final Logger log = LoggerFactory.getLogger(GuessingGameSpeechlet.class);
+
+/**
+ * Custom Slot key for the name animal given by the user
+ */
+private static final String ANIMALS = "Animals";
+
+
+/*This will need to be replaced with sessions*/
+public String correctAnimal = "ardvark";
+public int numPoints = 10;
+public int numStrikes = 0;
+/**/
+
 
 @Override
 public void onSessionStarted(final SessionStartedRequest request, final Session session)
@@ -145,7 +63,9 @@ public SpeechletResponse onIntent(final IntentRequest request, final Session ses
     //change based on intents
     if ("PlayGameIntent".equals(intentName)) {
         return playGameResponse();
-    } else if ("AMAZON.HelpIntent".equals(intentName)) {
+    }else if("GuessAnimalIntent".equals(intentName)) {
+    		return handleGuessTheAnimal(intent, session);
+	}else if ("AMAZON.HelpIntent".equals(intentName)) {
         return getHelpResponse();
     } else {
         throw new SpeechletException("Invalid Intent");
@@ -166,11 +86,11 @@ public void onSessionEnded(final SessionEndedRequest request, final Session sess
  * @return SpeechletResponse spoken and visual response for the given intent
  */
 private SpeechletResponse getWelcomeResponse() {
-    String speechText = "Welcome to the Alexa Skills Kit, you can say hello";
+    String speechText = "Welcome to the Guessing Game. You can say, \"Let's play.\"";
 
     // Create the Simple card content.
     SimpleCard card = new SimpleCard();
-    card.setTitle("HelloWorld");
+    card.setTitle("Welcome");
     card.setContent(speechText);
 
     // Create the plain text output.
@@ -185,36 +105,16 @@ private SpeechletResponse getWelcomeResponse() {
 }
 
 /**
- * Creates a {@code SpeechletResponse} for the hello intent.
- *
- * @return SpeechletResponse spoken and visual response for the given intent
- */
-private SpeechletResponse getHelloResponse() {
-    String speechText = "Hello world";
-
-    // Create the Simple card content.
-    SimpleCard card = new SimpleCard();
-    card.setTitle("HelloWorld");
-    card.setContent(speechText);
-
-    // Create the plain text output.
-    PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
-    speech.setText(speechText);
-
-    return SpeechletResponse.newTellResponse(speech, card);
-}
-
-/**
  * Creates a {@code SpeechletResponse} for the help intent.
  *
  * @return SpeechletResponse spoken and visual response for the given intent
  */
 private SpeechletResponse getHelpResponse() {
-    String speechText = "You can say hello to me!";
+    String speechText = "In this game, I will play a sound and you will try to guess the animal that made that sound. To start, say \"Let's play\"";
 
     // Create the Simple card content.
     SimpleCard card = new SimpleCard();
-    card.setTitle("HelloWorld");
+    card.setTitle("Help");
     card.setContent(speechText);
 
     // Create the plain text output.
@@ -229,14 +129,70 @@ private SpeechletResponse getHelpResponse() {
 }
 
 private SpeechletResponse playGameResponse() {
-    String speechText = "<speak> Welcome to the Animal Sound Guessing Game"
+    String speechText = "<speak> Guess the animal that makes this sound: "
     		+ " <audio src=\"https://s3.amazonaws.com/final-project-mson/EuzargZH-cat-meow-2-cat-stevens-2034822903.mp3\" /> </speak>";
     
-    // Create the plain text output.
+    //Create reprompt
     SsmlOutputSpeech outputSpeech = new SsmlOutputSpeech();
     outputSpeech.setSsml(speechText);
 
-    return SpeechletResponse.newTellResponse(outputSpeech);
+    Reprompt reprompt = new Reprompt();
+    reprompt.setOutputSpeech(outputSpeech);
+    SpeechletResponse response = SpeechletResponse.newAskResponse(outputSpeech, reprompt);
+    return response;
+}
+
+private SpeechletResponse handleGuessTheAnimal(Intent intent, final Session session) {
+	// get the type of animal guessed
+    Slot animalSlot = intent.getSlot(ANIMALS);
+    
+    String speech = "";
+    if (animalSlot != null && animalSlot.getValue() != null) {
+        String animalName = animalSlot.getValue();
+        
+        //If the animal guessed is correct
+        if(animalName == correctAnimal) {
+        		speech = "Good job! That's correct.";
+        		numPoints += 10;
+        		//If you've gotten enough points to level up
+                if(numPoints == 10 || numPoints == 20) {
+                		speech+="Nice work! You've leveled up! Now the animal sounds will be harder to guess.";
+                }
+        		playGameResponse();
+        }
+        //If the animal guessed is incorrect
+        if(animalName != correctAnimal) {
+        		speech= "Sorry, That's not correct.";
+        		numStrikes++;
+        		 //If you've gotten three strikes
+                if(numStrikes == 3) {
+                		speech+="Game over! You got " + numPoints + " points.";
+                }
+                else
+                		playGameResponse();
+        }
+        
+        SsmlOutputSpeech outputSpeech = new SsmlOutputSpeech();
+        outputSpeech.setSsml(speech);
+        
+        Reprompt reprompt = new Reprompt();
+        reprompt.setOutputSpeech(null);
+        SpeechletResponse response = SpeechletResponse.newAskResponse(outputSpeech, reprompt);
+        return response;
+        
+    }
+    else {
+    	// Create the plain text output.
+        
+        speech = "Sorry, I've never heard of that animal.";
+
+        SsmlOutputSpeech outputSpeech = new SsmlOutputSpeech();
+        outputSpeech.setSsml(speech);
+        
+        Reprompt reprompt = new Reprompt();
+        reprompt.setOutputSpeech(null);
+        SpeechletResponse response = SpeechletResponse.newAskResponse(outputSpeech, reprompt);
+        return response;
+    }
 }
 }
->>>>>>> Stashed changes
