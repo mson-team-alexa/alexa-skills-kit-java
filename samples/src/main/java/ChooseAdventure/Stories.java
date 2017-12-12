5b40d37da64ec6b4daa0fbd0b2d391567d4730ee
sassmith809 <sassmith809@gmail.com>
2017-12-12 11:39:29 -0500

    made choice

--- samples/src/main/java/ChooseAdventure/Stories.java
+++ samples/src/main/java/ChooseAdventure/Stories.java
@@ -36,19 +36,19 @@ public static Adventures getName(String name) {
     	ArrayList<Scenario> scenarios = new ArrayList<Scenario>();
  
     	
-    	scenarios.add(new Scenario("To follow the coordinates, say Follow" , "To follow your gut and go another way, say My Way" ,  "outcome"));
+    	scenarios.add(new Scenario("Follow" , "My Way" ,  "outcome"));
     	
-    	scenarios.add(new Scenario("To scan the black hole for life, say Scan ", "To orbit the black hole and search for nearby planets, say Search", "outcome"));
+    	scenarios.add(new Scenario("Scan ", "Search", "outcome"));
    
-    	scenarios.add(new Scenario("To translate the message, say Translate", "To fix the power, say Fix",  "outcome"));
+    	scenarios.add(new Scenario("Translate", "Fix",  "outcome"));
     	
-    	scenarios.add(new Scenario("To fix the engines, say Fix", "To figure out why the engines lost power, say Engines", "outcome"));
+    	scenarios.add(new Scenario("Fix", "Engines", "outcome"));
     	
-    	scenarios.add(new Scenario("To scan the planet for life, say Scan", "To search for more planets, say Search",  "outcome"));
+    	scenarios.add(new Scenario("Scan", "Search",  "outcome"));
     	
-    	scenarios.add(new Scenario("To fix the power and escape, say Fix", "To send a peace letter to the aliens, say Letter",  "outcome"));
+    	scenarios.add(new Scenario("Fix", "Letter",  "outcome"));
     	
-    	scenarios.add(new Scenario("To find the threat, say Find", "To leave in the escape pod, say Leave",  "outcome"));
+    	scenarios.add(new Scenario("Find", "Leave",  "outcome"));
     	
 
     	
